47e7b9ba7dc8ec443330ba22f5f6cb67a6e4f3ae
Huy Chiến <chienhi912@gmail.com>
2024-06-17 08:00:07 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api', (req, res) => {
     const url = 'http://14.225.255.190:8081' + req.url;
     req.pipe(request({ qs: req.query, uri: url })).pipe(res);
@@ -11,6 +17,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Proxy server listening on port ${port}`);
 });
-
-
-
