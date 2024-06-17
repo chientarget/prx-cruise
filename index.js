5d7f1eb0f6eec9a9e4a7b9cfb43d481031031cb5
Huy Chiến <chienhi912@gmail.com>
2024-06-17 08:04:00 +0700

    update

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
+// Middleware để thêm các header CORS
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+// Proxy các yêu cầu đến /api
 app.use('/api', (req, res) => {
     const url = 'http://14.225.255.190:8081' + req.url;
     req.pipe(request({ qs: req.query, uri: url })).pipe(res);
+});
+
+// Route mặc định cho các yêu cầu khác
+app.use((req, res) => {
+    res.status(404).send('Not Found');
 });
 
 const port = process.env.PORT || 3000;
